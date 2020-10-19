3add85da821903fb91513b157bf4e22d016b8342
Andrew Bernauer <bernaue2@illinois.edu>
2020-10-19 11:39:00 -0500

    added goals command to the bot

--- bot.py
+++ bot.py
@@ -3,17 +3,37 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
+description = '''An example dangbot for Dan Gheesling's discord server or guild
+
+
+There are a number of utility commands shown here.'''
+
 
 # get the path of the directory this files is in
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# establish the intents of the commands for the bot
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
 
-@client.event
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-------')
 
-client.run(TOKEN)
+
+@bot.command()
+async def goals(ctx):
+    """Returns the three goals of the 596"""
+    await ctx.send("1. clean. 2. positive. 3. entertainment".upper())
+
+bot.run(TOKEN)
